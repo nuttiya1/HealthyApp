a24976af3196fa968c81800e8b6f87c99b139193
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-18 21:32:38 +0700

    Update tests.py

--- fitjang/tests.py
+++ fitjang/tests.py
@@ -1,19 +1,17 @@
-from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.urls import resolve
+from fitjang.views import homepage
 
-from fitjang.views import home_page  
+# Create your tests here.
 
 class HomePageTest(TestCase):
+    def test_root_hotme_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, homepage)
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')  
-        self.assertEqual(found.func, home_page)
-
-    #def test_home_page_returns_correct_html(self):
-    #    request = HttpRequest()  
-    #    response = home_page(request)  
-    #    html = response.content.decode('utf8')  
-    #    self.assertTrue(html.startswith('<html>'))  
-    #    self.assertIn('<title>To-Do lists</title>', html)  
-    #    self.assertTrue(html.endswith('</html>'))    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = homepage(request) 
+        #self.assertTrue(response.content.startswith(b'<html>')) 
+        #self.assertTrue(response.content.endswith(b'</html>'))
