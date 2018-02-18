3a14d47aecfeb2ae357dab185245383f99aaecb4
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-18 23:15:50 +0700

    Update functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,8 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-#    def setUp(self):
-#        self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
 #    def tearDown(self):
 #        self.browser.quit()
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8080')
+        self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('Fit Jang', self.browser.title)
