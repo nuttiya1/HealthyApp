081dc67e08c1872a08f935954695889c0f45179d
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-18 22:53:31 +0700

    Update urls.py

--- fitjang/urls.py
+++ fitjang/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('adddata/', views.add_data, name='add_data'),
+    path('adddata/show/', views.show, name='show'),
 ]
