e152f2bea393f70c900b7df0953677e631e9ec9f
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-18 22:53:59 +0700

    Update tests.py

--- fitjang/tests.py
+++ fitjang/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from django.urls import resolve
-from fitjang.views import homepage
+from fitjang.views import homepage, add_data, show
 
 # Create your tests here.
 
@@ -12,6 +12,14 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        response = homepage(request) 
-        #self.assertTrue(response.content.startswith(b'<html>')) 
+        response = homepage(request)
+        #self.assertTrue(response.content.startswith(b'<html>'))
         #self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_add_data_returns_correct_html(self):
+            request = HttpRequest()
+            response = add_data(request)
+
+    def test_show_returns_correct_html(self):
+            request = HttpRequest()
+            response = show(request)
