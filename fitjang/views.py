76afeecb44c65dd44ddc495cfb4fe93ea75513a0
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-22 01:56:18 +0700

    try add activity

--- fitjang/views.py
+++ fitjang/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from fitjang.models import Activity
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    act = Activity()
+    act.activity_text = request.POST.get('act_text', '')
+    act.save()
+
+
+#    if request.method == 'POST':
+#        Activity.objects.create(text=request.POST.get['activity_text'])
+
+    return render(request, 'homepage.html', {'new_activity': act.activity_text})
+
+#def new_data(request):
+#    new_list = List.objects.create()
+#    Text.objects.create(text=request.POST['data_text'], list=new_list)
+
+#def add_data(request, data_id):
+#    new_list = List.objects.get(id=data_id)
+#    Data.objects.create(text=request.POST['data_text'], list=new_list)
 
 def show(request):
+#    new_list = List.objects.get(id=data_id)
     return render(request, 'show.html')
