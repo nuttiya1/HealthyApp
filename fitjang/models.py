7624dda375bc3094f6cc798e087037cda34606a2
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-22 00:09:52 +0700

    Update models.py
    add class List and Data

--- fitjang/models.py
+++ fitjang/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
-class Activity(models.Model):
-    activity_text = models.CharField(max_length=200)
+#class Activity(models.Model):
+#   activity_text = models.CharField(max_length=200)
+
+class List(models.Model):
+    pass
+
+class Data(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
+# Anyone has there own information such as name weight
+# activity
+
+#class Person(models.Model):
+#    first_name = models.CharField(max_length=30)
+#    last_name = models.CharField(max_length=30)
+#    activity_text = models.CharField(max_length=20)
