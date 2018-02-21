66e46b07ee68529d130d8e239baf14058169ef09
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-22 01:57:29 +0700

    test count object in Activity

--- fitjang/tests.py
+++ fitjang/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from django.urls import resolve
-from fitjang.views import homepage, add_data, show
+from fitjang.views import homepage, show
+from fitjang.models import Activity
 
 # Create your tests here.
 
@@ -18,16 +19,8 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
 
-        self.cilent.post('/', data={'activity_text': 'Mac donold' })
-
+        self.client.get('/', data={'activity_text': 'Mac donold' })
         self.assertEqual(Activity.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
 
-    def test_add_data_returns_correct_html(self):
-            request = HttpRequest()
-            response = add_data(request)
-
-    def test_show_returns_correct_html(self):
-            request = HttpRequest()
-            response = show(request)
+        #new_item = Activity.objects.first()
+        #self.assertEqual(new_item.activity_text, 'Mac donold')
