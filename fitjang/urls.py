f2ded0ac5692d8273c464b1eab85627c57a92539
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-02-22 00:09:03 +0700

    Update urls.py
    delete add_data

--- fitjang/urls.py
+++ fitjang/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('adddata/', views.add_data, name='add_data'),
     path('adddata/show/', views.show, name='show'),
 ]
