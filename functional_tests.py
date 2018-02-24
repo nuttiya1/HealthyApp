22d9fef1446ede17a94f1a4ad69462da688aed5a
nattapong <a5720187@gmail.com>
2018-02-24 17:09:03 +0700

    add function chack input in functional test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
        self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -34,10 +39,19 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
+        self.check_for_row_in_list_table('1: Run')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Run', [row.text for row in rows])
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_activity')
+        inputbox.send_keys('Wetgth')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Run')
+        self.check_for_row_in_list_table('2: Wetgth')
 
         self.fail('Finish the test!')
 
