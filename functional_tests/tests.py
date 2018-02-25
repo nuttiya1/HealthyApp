32312ab311cc37ff96e3a1093a91dce628445dc4
nattapong <a5720187@gmail.com>
2018-02-25 22:01:10 +0700

    edit format functional test and template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,10 +11,14 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
        self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text, row_weigth, row_time):
+    def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows_text = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows_text])
+        # row_weigth = table.find_elements_by_tag_name('tr')
+        # self.assertIn(row_weigth, [row.text for row in row_weigth])
+        # row_time = table.find_elements_by_tag_name('tr')
+        # self.assertIn(row_time, [row.text for row in row_time])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online health app. She goes
@@ -46,7 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.find_element_by_id("id_sub").click()
         time.sleep(2)
 
-        self.check_for_row_in_list_table('1: Run','60 กก.','120 นาที')
+        self.check_for_row_in_list_table('1: Run 60 กก. 120 นาที')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -64,7 +68,7 @@ class NewVisitorTest(LiveServerTestCase):
         # time.sleep(2)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Run','60 กก.','120 นาที')
+        self.check_for_row_in_list_table('1: Run 60 กก. 120 นาที')
         # self.check_for_row_in_list_table('2: Wetgth')
 
         self.fail('Finish the test!')
